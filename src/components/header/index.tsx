dacc1da4cb47c6190cfeee7680e163d66df6ce9b
HerbertKayky <herbertkayky9@gmail.com>
2024-05-14 16:43:12 -0300

    finalizado tela inicial e header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="w-full h-16 bg-slate-950 flex items-center justify-center">
+      <section className="w-full max-w-5xl flex p-5 justify-between items-center">
+        <nav className="flex items-center">
+          <Link className="text-white" href="/">
+            <h1 className="text-3xl font-bold">
+              Tarefas <span className="text-orange-600">+</span>
+            </h1>
+          </Link>
+          <Link
+            className="py-1 px-2 ml-3 rounded bg-slate-100"
+            href="/dashboard"
+          >
+            Meu painel
+          </Link>
+        </nav>
+        <button className="bg-transparent border px-8 py-1 font-bold rounded-lg text-white hover:scale-105 hover:text-black hover:bg-white  transition-all">
+          Acessar
+        </button>
+      </section>
+    </header>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { Header } from "@/components/header";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SessionProvider>
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  );
 }
