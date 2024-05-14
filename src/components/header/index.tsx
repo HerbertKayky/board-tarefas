b4c96bdb1a34dc768ced2040609fad099b5dd344
HerbertKayky <herbertkayky9@gmail.com>
2024-05-14 19:18:54 -0300

    adicionado funcionalidade de login e logout ao clicar no botão do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,9 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export function Header() {
+  const { data: session, status } = useSession();
+
   return (
     <header className="w-full h-16 bg-slate-950 flex items-center justify-center">
       <section className="w-full max-w-5xl flex p-5 justify-between items-center">
@@ -17,9 +20,23 @@ export function Header() {
             Meu painel
           </Link>
         </nav>
-        <button className="bg-transparent border px-8 py-1 font-bold rounded-lg text-white hover:scale-105 hover:text-black hover:bg-white  transition-all">
-          Acessar
-        </button>
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button
+            onClick={() => signOut()}
+            className="bg-transparent border px-8 py-1 font-bold rounded-lg text-white hover:scale-105 hover:text-black hover:bg-white  transition-all"
+          >
+            Olá {session?.user?.name}
+          </button>
+        ) : (
+          <button
+            onClick={() => signIn("google")}
+            className="bg-transparent border px-8 py-1 font-bold rounded-lg text-white hover:scale-105 hover:text-black hover:bg-white  transition-all"
+          >
+            Acessar
+          </button>
+        )}
       </section>
     </header>
   );
