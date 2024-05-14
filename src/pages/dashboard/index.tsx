d1246f7b72ad63bf5854f0f9f3efad51ebb86ba4
HerbertKayky <herbertkayky9@gmail.com>
2024-05-14 20:17:08 -0300

    adicionado proteção de rotas privadas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,33 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+
 export default function Dashboard() {
-    return(
-        <div>
-            <h1 className="text-5xl">Dashboard</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
+      <h1 className="text-5xl">Dashboard</h1>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  //console.log(session)
+
+  if (!session?.user) {
+    // Se não tem usuários vamos redirecionar para a "/"
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
